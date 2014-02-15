65f3be1d44934920adb48a0a36f491560795673a
Eric Perez <ericricardoperez@gmail.com>
2014-02-14 16:52:28 -0800

    generate library, page, and adventure controllers

---
+++ app/controllers/adventures_controller.rb
@@ -0,0 +1,10 @@
+class AdventuresController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
